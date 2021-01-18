4344af11c0e0ef32bf2b2f49790376f0c8955908
mashafi <mashafi.tech@gmail.com>
2021-01-18 12:13:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Project 2: Create a React App, Deploy on Surge & CICD</h1>
+    <h1>Project 2: Create a React App, Deploy on Surge, CICD</h1>
   );
 }
 
